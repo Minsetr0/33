0e608bc8cd579bb3d24979c0c0a5f303c1817f49
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 20:36:43 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Как вас зовут?");
+string name = Console.ReadLine();
+Console.WriteLine("Какой у вас знак зодиака?");
+string zodiacSign = Console.ReadLine();
+Console.WriteLine("Кем вы работаете?");
+string work = Console.ReadLine();
+Console.WriteLine($"{name}{zodiacSign}{work}");
