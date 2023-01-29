92121fa0674206ea630d23cec2c6bc8f1dd14eeb
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 21:51:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
-﻿Console.WriteLine("Как вас зовут?");
-string name = Console.ReadLine();
-Console.WriteLine("Какой у вас знак зодиака?");
-string zodiacSign = Console.ReadLine();
-Console.WriteLine("Кем вы работаете?");
-string work = Console.ReadLine();
-Console.WriteLine($"{name}{zodiacSign}{work}");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
